ac5e59475851947f55a89d241a06b72c0f84a428
emirhantarlabasi <147060954+emirhantarlabasi@users.noreply.github.com>
2024-11-08 13:30:04 +0300

    Update ItemData.kt

--- src/main/java/com/example/firstex/ItemData.kt
+++ src/main/java/com/example/firstex/ItemData.kt
@@ -3,8 +3,9 @@ package com.example.firstex
 import java.io.Serializable
 
 data class ItemData(
+    val id: Int,
     val title: String,
     val description: String,
-    val imageUrl: String,
-    val id: Int
-) : Serializable
+    val smallImageUrl: String,
+    val largeImageUrl: String
+)
